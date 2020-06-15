9b26999a620b1167af319f79a752f5e3fd1ed77b
Pelegrinetti <pelegrinetti.dev@gmail.com>
2020-06-14 23:16:36 -0300

    add time picker dialog

--- app/src/main/java/com/pelegrinetti/onix/TaskActivity.java
+++ app/src/main/java/com/pelegrinetti/onix/TaskActivity.java
@@ -2,13 +2,63 @@ package com.pelegrinetti.onix;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
+import android.icu.util.Calendar;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+
+import java.util.Objects;
 
 public class TaskActivity extends AppCompatActivity {
+    Button btnPickTime;
+    String pickedTime;
+    Bundle bundle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
+
+        loadWidgets();
+
+        setEvents();
+    }
+
+    public void loadWidgets() {
+        btnPickTime = (Button) findViewById(R.id.task_time);
+        bundle = getIntent().getExtras();
+    }
+
+    public void setEvents() {
+        btnPickTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar c = Calendar.getInstance();
+
+                int hour, minute;
+
+                hour = c.get(Calendar.HOUR);
+                minute = c.get(Calendar.MINUTE);
+
+                TimePickerDialog timePickerDialog = new TimePickerDialog(TaskActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        String[] date = Objects.requireNonNull(bundle.getString("date")).split(" ");
+
+                        pickedTime = date[0] +
+                                " " +
+                                (hourOfDay <= 9 ? "0" + String.valueOf(hourOfDay) : String.valueOf(hourOfDay)) +
+                                ":" +
+                                (minute <= 9 ? "0" + String.valueOf(minute) : String.valueOf(minute)) +
+                                ":00";
+                    }
+                }, hour, minute, true);
+
+                timePickerDialog.show();
+            }
+        });
     }
 }
