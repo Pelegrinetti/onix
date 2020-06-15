15aae9ef900d9bba3ed6ec30e764bb343e872506
Pelegrinetti <pelegrinetti.dev@gmail.com>
2020-06-14 23:21:04 -0300

    load all task activity widgets

--- app/src/main/java/com/pelegrinetti/onix/TaskActivity.java
+++ app/src/main/java/com/pelegrinetti/onix/TaskActivity.java
@@ -7,13 +7,15 @@ import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TimePicker;
 
 import java.util.Objects;
 
 public class TaskActivity extends AppCompatActivity {
-    Button btnPickTime;
+    Button btnPickTime, btnSaveTask;
     String pickedTime;
+    EditText txtTaskTitle, txtTaskDescription;
     Bundle bundle;
 
 
@@ -29,6 +31,9 @@ public class TaskActivity extends AppCompatActivity {
 
     public void loadWidgets() {
         btnPickTime = (Button) findViewById(R.id.task_time);
+        btnSaveTask = (Button) findViewById(R.id.task_save);
+        txtTaskTitle = (EditText) findViewById(R.id.task_title);
+        txtTaskDescription = (EditText) findViewById(R.id.task_description);
         bundle = getIntent().getExtras();
     }
 
