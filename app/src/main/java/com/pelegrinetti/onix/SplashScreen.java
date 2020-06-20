c95832f56d8623c87f7c3d701eef851563dfd15d
Pelegrinetti <pelegrinetti.dev@gmail.com>
2020-06-20 15:36:18 -0300

    add splash screen logic

--- app/src/main/java/com/pelegrinetti/onix/SplashScreen.java
+++ app/src/main/java/com/pelegrinetti/onix/SplashScreen.java
@@ -2,7 +2,9 @@ package com.pelegrinetti.onix;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        int SPLASH_TIME_OUT = 1500;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this,
+                        MainActivity.class);
+                startActivity(i);
+
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
